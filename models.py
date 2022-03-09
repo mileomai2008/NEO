157688b4df249f1c0769806555c951ef76674635
MayGhonimy <86968416+MayGhonimy@users.noreply.github.com>
2022-03-09 11:15:42 +0200

    first commit

---
+++ models.py
@@ -0,0 +1,137 @@
+"""Represent models for near-Earth objects and their close approaches.
+
+The `NearEarthObject` class represents a near-Earth object. Each has a unique
+primary designation, an optional unique name, an optional diameter, and a flag
+for whether the object is potentially hazardous.
+
+The `CloseApproach` class represents a close approach to Earth by an NEO. Each
+has an approach datetime, a nominal approach distance, and a relative approach
+velocity.
+
+A `NearEarthObject` maintains a collection of its close approaches, and a
+`CloseApproach` maintains a reference to its NEO.
+
+The functions that construct these objects use information extracted from the
+data files from NASA, so these objects should be able to handle all of the
+quirks of the data set, such as missing names and unknown diameters.
+
+"""
+from helpers import cd_to_datetime, datetime_to_str
+
+
+class NearEarthObject:
+    """A near-Earth object (NEO).
+
+    An NEO encapsulates semantic and physical parameters about the object, such
+    as its primary designation (required, unique), IAU name (optional), diameter
+    in kilometers (optional - sometimes unknown), and whether it's marked as
+    potentially hazardous to Earth.
+    A `NearEarthObject` also maintains a collection of its close approaches -
+    initialized to an empty collection, but eventually populated in the
+    `NEODatabase` constructor.
+    """
+
+    def __init__(self, designation, name=None, diameter=float('nan'), hazardous=False):
+        """Create a new `NearEarthObject`.
+
+        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        """
+        self.designation = str(designation)
+        if name:
+            self.name = name
+        else:
+            self.name = None
+        try:
+            self.diameter = float(diameter)
+        except:
+            self.diameter = float('nan')
+        if hazardous:
+            self.hazardous = True
+        else:
+            self.hazardous = False
+
+        # Create an empty initial collection of linked approaches.
+        self.approaches = []
+
+    @property
+    def fullname(self):
+        """Return a representation of the full name of this NEO."""
+        if self.name:
+            return f'NEO [Designation: {self.designation!r} - Name: {self.name!r}]'
+        else:
+            return f'NEO [Designation: {self.designation!r} ]'
+
+    def __str__(self):
+        """Return `str(self)`."""
+        if self.hazardous:
+            hazardous_var = ""
+        else:
+            hazardous_var = "not "
+
+        return f"{self.fullname} with a diameter equal to {self.diameter:.3f} KM and it's {hazardous_var}" \
+               f"potentially hazardous"
+
+    def __repr__(self):
+        """Return `repr(self)`, a computer-readable string representation of this object."""
+        return f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, " \
+               f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
+
+
+class CloseApproach:
+    """A close approach to Earth by an NEO.
+
+    A `CloseApproach` encapsulates information about the NEO's close approach to
+    Earth, such as the date and time (in UTC) of closest approach, the nominal
+    approach distance in astronomical units, and the relative approach velocity
+    in kilometers per second.
+    A `CloseApproach` also maintains a reference to its `NearEarthObject` -
+    initially, this information (the NEO's primary designation) is saved in a
+    private attribute, but the referenced NEO is eventually replaced in the
+    `NEODatabase` constructor.
+    """
+
+    def __init__(self, time, distance=0, velocity=0, designation=None, neo=None):
+        """Create a new `CloseApproach`.
+
+        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        """
+        self._designation = designation
+        self.time = cd_to_datetime(time)
+        try:
+            self.distance = float(distance)
+        except:
+            self.distance = float('nan')
+        try:
+            self.velocity = float(velocity)
+        except:
+            self.velocity = float('nan')
+        self.neo = neo
+
+    @property
+    def time_str(self):
+        """Return a formatted representation of this `CloseApproach`'s approach time.
+
+        The value in `self.time` should be a Python `datetime` object. While a
+        `datetime` object has a string representation, the default representation
+        includes seconds - significant figures that don't exist in our input
+        data set.
+
+        The `datetime_to_str` method converts a `datetime` object to a
+        formatted string that can be used in human-readable representations and
+        in serialization to CSV and JSON files.
+        """
+        return datetime_to_str(self.time)
+
+    def __str__(self):
+        """Return `str(self)`."""
+        if self.neo:
+            neo_var = f"({self.neo})"
+        else:
+            neo_var = f" NEO [{self._designation}]"
+        return f"A CloseApproach: At {self.time_str} {neo_var} approaches Earth at " \
+               f"a distance of {self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s"
+
+    def __repr__(self):
+        """Return `repr(self)`, a computer-readable string representation of this object."""
+        return f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, " \
+               f"velocity={self.velocity:.2f}, neo={self.neo!r})"
